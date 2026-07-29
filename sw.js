84deca2c3e3a5391d799ba1f30531fe9a4ec2f1e
Braunic1221 <121628118+Braunic1221@users.noreply.github.com>
2026-07-29 18:46:47 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='shinobi-fit-v13.5.5-github-20260726';
+const CACHE='shinobi-fit-v13.5.6-github-20260729';
 const ASSETS=['./','./index.html','./manifest.json','./icon.svg','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
 
 self.addEventListener('install',event=>{
